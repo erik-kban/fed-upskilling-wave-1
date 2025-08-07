5734f6333a750c17cca38be226aca566b32c42e8
Emmanuel Chu <emchu@deloitte.com.au>
2025-08-07 10:54:43 +1000

    feat: Add button sizes

--- src/components/atoms/CTAButton.tsx
+++ src/components/atoms/CTAButton.tsx
@@ -5,11 +5,12 @@ interface CTAButtonProps {
   icon?: React.ReactNode;
   text?: React.ReactNode;
   isDarkButton?: boolean;
+  size?: "small" | "medium" | "large"
 }
 
-const CTAButton: React.FC<CTAButtonProps> = ({ icon, text, isDarkButton }) => {
+const CTAButton: React.FC<CTAButtonProps> = ({ icon, text, isDarkButton, size }) => {
   return (
-    <button className={`${styles.ctaButton} ${isDarkButton ? styles["ctaButton--dark"] :styles["ctaButton--normal"]}`}>
+    <button className={`${styles.ctaButton} ${styles[`ctaButton--size--${size}`]} ${isDarkButton ? styles["ctaButton--dark"] :styles["ctaButton--normal"]}`}>
       {icon && icon}
       {text}
     </button>
