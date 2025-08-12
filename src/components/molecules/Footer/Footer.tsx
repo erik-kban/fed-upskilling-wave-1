341511e1c83f6d6c9550fdc189d6edf08373ccd0
ekaban <ekaban@deloitte.com.au>
2025-08-12 17:28:46 +1000

    FEAT: Completed FooterLinks and Footer

--- src/components/molecules/Footer/Footer.tsx
+++ src/components/molecules/Footer/Footer.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Disclaimers from '@/components/atoms/Disclaimers/Disclaimers'
 import Sponsors from '@/components/atoms/Sponsors/Sponsors'
+import FooterLinks from '@/components/atoms/FooterLinks/FooterLinks'
 
 const Footer = () => {
   return (
-    <>  
+    <>
+      <FooterLinks />
       <Sponsors />
-        <Disclaimers />
+      <Disclaimers />
     </>
   )
 }
