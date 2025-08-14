392d8dbafc03dd214839296f46a7ac0a23bf179f
Emmanuel Chu <emchu@deloitte.com.au>
2025-08-14 12:58:33 +1000

    FEAT: Create Product Categories molecule

---
+++ src/assets/icons/YellowIcon.tsx
@@ -0,0 +1,23 @@
+const YellowIcon = () => {
+  return (
+    <svg
+      viewBox="0 0 32 32"
+      xmlns="http://www.w3.org/2000/svg"
+      width="32.000000"
+      height="32.000000"
+      fill="none"
+    >
+      <rect
+        id="Rectangle 2828"
+        width="32.000000"
+        height="32.000000"
+        x="0.000000"
+        y="0.000000"
+        rx="4.000000"
+        fill="rgb(255,210,0)"
+      />
+    </svg>
+  );
+};
+
+export default YellowIcon;
